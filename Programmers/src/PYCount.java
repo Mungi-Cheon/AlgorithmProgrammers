92370cb61d6db7d20eb7e35a7adb399ec3e28ea2
MungiCheon <c.mungi7421@gmail.com>
2024-02-27 20:18:16 +0900

    프로그래머스 -  문자열 내 p와 y의 개수 풀이 등록

---
+++ Programmers/src/PYCount.java
@@ -0,0 +1,11 @@
+public class PYCount {
+    public static void main(String[] args) {
+        String s = "Pyy";
+        
+        String upper = s.toUpperCase();
+        int pCnt = upper.length() - upper.replaceAll("P", "").length();
+        int yCnt = upper.length() - upper.replaceAll("Y", "").length();
+
+        System.out.println(pCnt == yCnt);
+    }
+}
