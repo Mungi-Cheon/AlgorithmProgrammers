b6e3fee6a9de0395e60da1ac73601df275e877b7
MungiCheon <c.mungi7421@gmail.com>
2024-03-16 14:50:13 +0900

    프로그래머스 -  최소 직사각형 풀이 등록

---
+++ Programmers/src/SmallestRectangle.java
@@ -0,0 +1,32 @@
+public class SmallestRectangle {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        
+        int width = 0;
+        int height = 0;
+        int maxWidth = 0;
+        int maxHeight = 0;
+        for(int[] size : sizes){
+            // 가로 사이즈와 세로 사이즈를 교환
+            width = Math.max(size[0],size[1]);
+            height = Math.min(size[0],size[1]);
+            
+            // 더 큰 값으로 계속 교환
+            maxWidth = Math.max(maxWidth, width);
+            maxHeight = Math.max(maxHeight, height);
+        }
+
+        answer = maxWidth * maxHeight;
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        
+        // int[][] sizes = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}   
+        // int[][] sizes = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}}
+
+        SmallestRectangle rectangle = new SmallestRectangle();
+        System.out.println("result = " +rectangle.solution(sizes));
+    }
+}
