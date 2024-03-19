8424eeaaaac0b677ac4e8cac7d9b764a57a17c8b
MungiCheon <c.mungi7421@gmail.com>
2024-03-20 01:00:01 +0900

    프로그래머스 -  크기가 작은 부분 문자열 풀이 등록

---
+++ Programmers/src/SmallSubstring.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class SmallSubstring {
+
+    public int solution(String t, String p) {
+        int answer = 0;
+        for(int i = 0; i <= t.length()- p.length(); i++){
+            int num = Integer.parseInt(t.substring(i, i + p.length()));
+            
+            if(num <= Integer.parseInt(p)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = null;
+        try 
+        {
+            sc = new Scanner(System.in);
+            System.out.print("t의 값 : ");
+            String t = sc.nextLine();
+            
+            System.out.print("p의 값 : ");
+            String p = sc.nextLine();
+
+            SmallSubstring smallSubstring = new SmallSubstring();
+            System.out.println(smallSubstring.solution(t, p));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            // 스캐너 클로즈
+            if(sc != null){
+                sc.close();
+            }
+        }
+    }
+}
