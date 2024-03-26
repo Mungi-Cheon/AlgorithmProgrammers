39940d3d5c75bcdc9eaa14798bac693237bfb361
MungiCheon <c.mungi7421@gmail.com>
2024-03-26 15:59:07 +0900

    프로그래머스 -  시저 암호 풀이 등록

---
+++ Programmers/src/CaesarCipher.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class CaesarCipher {
+
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        for(char c : s.toCharArray()){
+            if(c == 32) { // 스페이스
+                answer += c;
+                continue;
+            }else if( c <= 90) { // 대문자 
+                c += n; // n만큼 밀기
+                
+                if(c >= 91){ // Z의 아스키 코드를 초과했을때 26만큼 빼기
+                    c -= 26;
+                }
+            }else{ // 소문자
+                c += n; // n만큼 밀기
+                
+                if( c >= 123 ){ // z의 아스키 코드를 초과했을때 26만큼 빼기
+                    c -= 26;
+                }
+            }
+            answer += c;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = null;
+        try 
+        {
+            sc = new Scanner(System.in);
+            System.out.print("s : ");
+            String s = sc.nextLine();
+            
+            System.out.print("n : ");
+            int n = sc.nextInt();
+
+            CaesarCipher app = new CaesarCipher();
+            String result = app.solution(s, n);
+            System.out.println(result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            // 스캐너 클로즈
+            if(sc != null){
+                sc.close();
+            }
+        }
+    }
+}
