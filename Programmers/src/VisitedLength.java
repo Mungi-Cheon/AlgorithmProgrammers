faa800f9d3701a89f94fd3c8053378551431de29
Mungi Cheon <c.mungi7421@gmail.com>
2024-04-17 23:44:46 +0900

    프로그래머스 -  방문 길이  풀이 등록

---
+++ Programmers/src/VisitedLength.java
@@ -0,0 +1,58 @@
+public class VisitedLength {
+
+    public int solution(String dirs) {
+        int answer = 0;
+    
+        boolean[][][][] visited = new boolean[11][11][11][11];
+        int curRow = 5, curCol = 5;
+        
+        for(char ch : dirs.toCharArray()) {
+            
+            int nextRow = curRow , nextCol = curCol;
+
+            if(ch == 'U'){
+                nextRow = curRow + 1; 
+            }else if(ch == 'D'){
+                nextRow = curRow - 1;
+            }else if(ch == 'R'){
+                nextCol = curCol + 1;
+            }else{
+                nextCol = curCol - 1;
+            }
+            // if(isValid(visited,nextRow,nextCol,direction)){
+            if(isValid(visited,curRow, curCol, nextRow,nextCol) && 
+            isDuplicate(visited, curRow, curCol, nextRow, nextCol)){
+                visited[nextRow][nextCol][curRow][curCol] = true;
+                visited[curRow][curCol][nextRow][nextCol] = true;                
+                answer++;
+            }
+
+            if(isValid(visited, curRow, curCol, nextRow, nextCol)){
+                curRow = nextRow;
+                curCol = nextCol;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isDuplicate(boolean[][][][]visited, int curRow, int curCol, int nextRow, int nextCol){
+        return !visited[curRow][curCol][nextRow][nextCol] && !visited[nextRow][nextCol][curRow][curCol];
+    }
+
+    private boolean isValid(boolean[][][][]visited, int curRow, int curCol, int nextRow, int nextCol){
+        return nextRow >= 0 && nextRow < visited.length && nextCol >= 0 && nextCol < visited[0].length;
+    }
+
+    // private boolean isValid(boolean[][][][] visited, int nextRow, int nextCol, int direction){
+    //     return nextRow >= 0 && nextRow < visited.length && nextCol >= 0 && nextCol < visited[0].length && !visited[nextRow][nextCol][direction];
+    // }
+
+    public static void main(String[] args) {
+        // String dirs="ULURRDLLU";
+        // String dirs="LULLLLLLU";
+        String dirs = "UDU";
+        VisitedLength app = new VisitedLength();
+        System.out.println(app.solution(dirs));
+    }
+}
